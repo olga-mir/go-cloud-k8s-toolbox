a47e422eb285a196f479d14d83db2a6972a862fa
Olga Mirensky <5200844+olga-mir@users.noreply.github.com>
2023-07-01 19:22:36 +1000

    hardcoded rules for RBAC resources and pods

--- cmd/k8s/rbac-helper.go
+++ cmd/k8s/rbac-helper.go
@@ -85,7 +85,6 @@ type RoleRule struct {
 }
 
 func RBACCompose() error {
-
 	// input file is output of kubectl api-resources but with the first line removed and
 	// removed the SHORTNAMES column manually
 	file, err := os.Open("api-resources.txt")
@@ -107,10 +106,15 @@ func RBACCompose() error {
 		Rules: []RoleRule{},
 	}
 
+	// Rules for core pods and RBAC will be hardcoded separately and should not be auto-imported from api-resources
+	sensitiveApiGroups := []string{"rbac.authorization.k8s.io"}
+	sensitiveCoreResources := []string{"pods", "secrets"}
+
 	prevApiGroup := "core" // TODO - assumes first line is core group
 	resources := []string{}
 	apigroup := ""
 	for scanner.Scan() {
+		// each line is from "k api-resources" but shortnames column should be removed manually from the input file
 		//  flowschemas                                    flowcontrol.apiserver.k8s.io/v1beta2   false        FlowSchema
 		line := scanner.Text()
 
@@ -124,17 +128,36 @@ func RBACCompose() error {
 		}
 
 		if apigroup != prevApiGroup {
-			rule := RoleRule{
-				APIGroups: []string{prevApiGroup},
-				Resources: resources,
-				Verbs:     []string{"*"},
+			if !contains(sensitiveApiGroups, prevApiGroup) {
+				rule := RoleRule{
+					APIGroups: []string{prevApiGroup},
+					Resources: resources,
+					Verbs:     []string{"*"},
+				}
+				role.Rules = append(role.Rules, rule)
 			}
-			role.Rules = append(role.Rules, rule)
 			resources = []string{}
 			prevApiGroup = apigroup
 		}
-		resources = append(resources, fields[0])
+
+		if !(prevApiGroup == "core" && contains(sensitiveCoreResources, fields[0])) {
+			resources = append(resources, fields[0])
+		}
 	}
+
+	podRule := RoleRule{
+		APIGroups: []string{"core"},
+		Resources: []string{"pods"},
+		Verbs:     []string{"get", "list", "watch", "create", "update", "patch", "delete"},
+	}
+	role.Rules = append(role.Rules, podRule)
+
+	rbacRule := RoleRule{
+		APIGroups: []string{"rbac.authorization.k8s.io"},
+		Resources: []string{"clusterrolebindings", "clusterroles", "rolebindings", "roles"},
+		Verbs:     []string{"get", "list"},
+	}
+	role.Rules = append(role.Rules, rbacRule)
 
 	if err := scanner.Err(); err != nil {
 		return err
@@ -154,4 +177,13 @@ func RBACCompose() error {
 
 	fmt.Println("ClusterRole written to clusterrole.yaml")
 	return nil
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
